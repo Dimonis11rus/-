4ba48df3a9619fde4d36da535e0572fa27a2c407
Dimonis11rus <43698838+Dimonis11rus@users.noreply.github.com>
2019-02-15 20:23:09 +0300

    Create ConcretePublisher.cpp

---
+++ ConcretePublisher.cpp
@@ -0,0 +1,108 @@
+#include "ConcretePublisher.h"
+#include <fstream>
+#include <iostream>
+#include <conio.h>
+#include "my_fun.cpp"
+#include "ISubscriber.h"
+#include "ConcreteSubscriber.h"
+using namespace std;
+
+ConcretePublisher::ConcretePublisher(char *a)
+{
+	count++;
+	address = nullptr;
+	clc = 0;
+
+	name = new char[25];
+	article = new char[600];
+	all_text_magazine = new char[5000];
+
+	for (int i = 0; i < 25; i++)
+	{
+		this->name[i] = a[i];
+	}
+	path = new char[25];
+	toTXT(name, path);
+
+	ifstream fin;
+	try
+	{
+		fin.open(path);
+		if (!fin.is_open())
+			throw 1;
+	}
+	catch (int i)
+	{
+		cout << "Отсутствует входной файл с ифнормацией о журнале " << name << endl;
+		exit(1);
+	}
+	int i = 0;
+	do
+	{
+		fin.get(all_text_magazine[i]);
+		i++;
+	} while (!fin.eof());
+	all_text_magazine[i - 1] = '\0';
+	fin.close();
+}
+
+void ConcretePublisher::get_latest_info()
+{
+	int i = 0;
+	do
+	{
+		article[i] = all_text_magazine[clc];
+		i++;	clc++;
+	} while (all_text_magazine[clc] != '#');
+	clc++;
+	article[i] = '\0';
+}
+
+void ConcretePublisher::subscribe(ISubscriber &subscriber)
+{
+	this->address = &subscriber;
+}
+
+void ConcretePublisher::unsubscribe()
+{
+	this->address = nullptr;
+}
+
+void ConcretePublisher::notify()
+{
+	if (address != nullptr)
+	{
+		address->update(name, article);
+	}
+}
+
+bool ConcretePublisher::plus_or_minus()
+{
+	return address != nullptr ? true : false;
+}
+
+void ConcretePublisher::some_logic(ConcretePublisher *a)
+{
+	const int kol_vo_statei = 10;
+	while (cntr_for_artcl<kol_vo_statei && !_kbhit())
+	{
+		for (int i = 0; i < count; i++)
+		{
+			a[i].get_latest_info();
+			a[i].notify();
+		}
+		Sleep(5000);
+		cout << "-------------------------------------------------------------------------------------------------------------------------" << endl;
+		cntr_for_artcl++;
+	}
+}
+
+int ConcretePublisher::count = 0;			// начальная инициализация статической переменной
+int ConcretePublisher::cntr_for_artcl = 0;
+
+ConcretePublisher::~ConcretePublisher()
+{
+	delete[] name;
+	delete[] article;
+	delete[] all_text_magazine;
+}
