eb463fc677e40852ae10b3663a1ce3c4b94b531f
Dimonis11rus <43698838+Dimonis11rus@users.noreply.github.com>
2019-02-23 02:05:18 +0300

    Create MyVector.h

---
+++ MyVector.h
@@ -0,0 +1,19 @@
+#pragma once
+
+template <class T>
+class MyVector
+{
+private:
+	int Size;
+	T *mas;
+public:
+	MyVector();
+	void Push_Back(const T&);
+	void Remove(const T&);
+	int ret_size();
+	void show();
+	bool is_there(const T&);
+	T& operator[](int);
+	~MyVector();
+};
+#include "MyVector.cpp"
