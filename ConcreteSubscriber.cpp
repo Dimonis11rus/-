3b6c48dc974735f242675b04b47cd53a1926d571
Dimonis11rus <43698838+Dimonis11rus@users.noreply.github.com>
2019-02-23 02:03:05 +0300

    Update ConcreteSubscriber.cpp

--- ConcreteSubscriber.cpp
+++ ConcreteSubscriber.cpp
@@ -2,17 +2,43 @@
 #include <windows.h>
 #include <iostream>
 #include <fstream>
+
 HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 using namespace std;
+ConcreteSubscriber::ConcreteSubscriber(char *a)
+{
+	counter++;
+	name = new char[25];
+	for (int i = 0; i < 25; i++)
+	{
+		this->name[i] = a[i];
+	}
+	path = new char[25];
+	toTXT(name, path);
+}
+char* ConcreteSubscriber::ret_name()
+{
+	return name;
+}
 void ConcreteSubscriber::update(char *name, char *article)
 {
 	SetConsoleTextAttribute(hConsoleHandle, 13);
-	cout << name;
+	cout << "Свежий выпуск журнала " << name << " для " << this->name << endl;
 	SetConsoleTextAttribute(hConsoleHandle, 15);
 	cout << article << endl;
 	ofstream fout; 
-	fout.open("OUTPUT.txt", ios::app);
+	fout.open(this->path, ios::app);
 	fout << name;
 	fout << article << endl;
 	fout.close();
 }
+void ConcreteSubscriber::clear_file()
+{
+	ofstream clear; clear.open(path);	clear.close();	//онли для того, чтобы отчистить выходной файл, после предыдущего запуска
+}
+ConcreteSubscriber::~ConcreteSubscriber()
+{
+	delete[] name;
+	delete[] path;
+}
+int ConcreteSubscriber::counter = 0;
